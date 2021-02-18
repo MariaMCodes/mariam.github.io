35e0de91ce457a36819c39e72afd507ef298b4bb
MMempin421 <maria.mempin@hotmail.com>
2021-02-18 13:12:14 +1100

    First commit

---
+++ scripts/static website.R
@@ -0,0 +1,44 @@
+
+library(shiny)
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      width = 3,
+      h2("My Static Website"),
+      h3("Internal link example"),
+      a(h4("Birds"),
+        href = "#birds"),
+      hr(),
+      h3("External link example"),
+      a(h4("Wikipedia"),
+        href = "https://en.wikipedia.org/wiki/Main_Page",
+        target = "blank")
+    ),
+    
+    mainPanel(
+      width = 9,
+      h2("Link to a saved sample.html"),
+      p("The url is https://johndoe.github.io/samples/sample.html"),
+      a(h3("Lovely Birds"),
+        href = "https://johndoe.github.io/samples/sample.html",
+        target = "blank"),
+      hr(),
+      h2("Text and image example", id = "birds"),
+      fluidRow(
+        column(
+          7,
+          includeMarkdown("markdown/birds.Rmd")
+        ),
+        column(
+          5,
+          img(src = "images/birds.png", width = "100%")
+        )
+      ),
+    )
+  )
+)
+
+# The serve.R file has an empty function as we do not have any interactive features in ui.R
+server <- function(input, output) {
+  # empty
+}
